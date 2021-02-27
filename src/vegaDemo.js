878c4f7bcf6cf30f1f69825fdb2aef5e8cfb8b18
Sunny Li <sunny@sunny0321deMacBook-Air.local>
2021-02-27 11:52:38 +0800

    merge

--- src/vegaDemo.js
+++ src/vegaDemo.js
@@ -1,8 +1,9 @@
-import fpData from '../static/fpData.csv'    // import dataset
+import fpData from '../static/temp-ghg-dataset.csv'    // import dataset
 "use strict";     // the code should be executed in "strict mode".
                   // With strict mode, you can not, for example, use undeclared variables
 
 var ds = [];   // used to store data later
+var countries = [];
 
 
 const options = {
@@ -26,45 +27,50 @@ const options = {
 vl.register(vega, vegaLite, options);
 
 // Again, We use d3.csv() to process data
-d3.csv(ds).then(function(data) {
+d3.csv(fpData).then(function(data) {
   data.forEach(function(d){
     ds.push(d);
+    if (!countries.includes(d.Country)) {
+      countries.push(d.Country);
+    }
   })
   drawBarVegaLite();
 });
 
-
 function drawBarVegaLite() {
   // var sunshine = add_data(vl, sunshine.csv, format_type = NULL);
   // your visualization goes here
-  const countries = uniqueValid(ds, d => d.Country)
     const selectGenre = vl.selectSingle('Select') // name the selection 'Select'
       .fields('Country')          // limit selection to the Major_Genre field
       .init({Country: countries[0]}) // use first genre entry as initial value
       .bind(vl.menu(countries));
 
-    const temp = vl.markLine()
+    const temp = vl.markBar()
       .data(ds)
       .select(selectGenre)
       .transform(
+        //vl.if(selectGenre, vl.filter('datum.Country == ' + selectGenre)),
         vl.groupby(['Country','Year'])
-          .aggregate(vl.average('temperature').as('avg_temp'))
+          .aggregate(vl.average('Temperature').as('avg_temp'))
       ).encode(
-        vl.y().fieldQ('avg_temp').title('avg_temp'),
+        vl.y().fieldQ('avg_temp').title('Temperature Change'),
         vl.x().fieldQ('Year'),
-        vl.opacity().if(selectGenre, vl.value(0.75)).value(0.05)
-      );
+        vl.opacity().if(selectGenre, vl.value(1)).value(0)
+      ).width(500)
+      .height(300);
 
     const ghg = vl.markBar()
       .data(ds)
       .transform(
+        vl.filter('datum.Pollutant == "Carbon dioxide"'),
         vl.groupby(['Country','Year'])
           .aggregate(vl.average('GHG').as('avg_ghg'))
       ).encode(
         vl.x().fieldQ('Year'),
-        vl.y().fieldQ('avg_ghg').title('avg_ghg'),
-        vl.opacity().if(selectGenre, vl.value(0.75)).value(0.05)
-      )
+        vl.y().fieldQ('avg_ghg').title('avg_carbon_dioxide'),
+        vl.opacity().if(selectGenre, vl.value(1)).value(0)
+      ).width(500)
+      .height(300);
 
     vl.vconcat(ghg, temp)
       .render()
@@ -73,4 +79,4 @@ function drawBarVegaLite() {
       // viewElement.value contains the Vega View object instance
       document.getElementById('view').appendChild(viewElement);
     });
-}
+  }
